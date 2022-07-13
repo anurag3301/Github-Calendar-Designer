d05bdd552adb8aa7f39d5170efe381c42e01d98d
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-07-13 19:47:52 +0530

    Added the main driver

---
+++ git_commenter/main.py
@@ -0,0 +1,16 @@
+import grid_ui
+import commiter
+import random
+
+YEAR = 2019
+REPO_DIR = '/home/anurag/repo/test'
+USERNAME = '0xpar'
+USEREMAIL = 'anurag3301.0x0@gmail.com'
+UPPER_LIMIT = 5
+LOWER_LIMIT = 2
+
+if __name__ == "__main__":
+    dates = grid_ui.get_dates(YEAR)
+    for i in dates:
+        for j in range(random.randint(LOWER_LIMIT, UPPER_LIMIT)):
+            commiter.commit(i)
